53e459d411fc47075aa694c61793b860913330f9
Vaibhav Kumar <105876297+Vai7983@users.noreply.github.com>
2023-12-09 17:21:15 +0530

    Update python_task_2.py

--- templates/python_task_2.py
+++ templates/python_task_2.py
@@ -11,10 +11,35 @@ def calculate_distance_matrix(df)->pd.DataFrame():
     Returns:
         pandas.DataFrame: Distance matrix
     """
-    # Write your logic here
+     # Read the CSV file into a DataFrame
+    df = pd.read_csv(datasets/dataset-3.csv)
 
-    return df
+    # Create a pivot table with id_1 and id_2 as index and columns, and distance as values
+    distance_matrix = df.pivot_table(index='id_1', columns='id_2', values='distance', fill_value=0)
 
+    # Make the matrix symmetric
+    distance_matrix = distance_matrix.add(distance_matrix.T, fill_value=0)
+
+    # Set diagonal values to 0
+    distance_matrix.values[[range(distance_matrix.shape[0])]*2] = 0
+
+    # Iterate to update cumulative distances
+    for col in distance_matrix.columns:
+        for row in distance_matrix.index:
+            if distance_matrix.loc[row, col] == 0 and row != col:
+                # Find intermediate points to calculate cumulative distance
+                intermediates = distance_matrix.loc[row].index.intersection(distance_matrix[col].index)
+                intermediates = intermediates[intermediates != row]
+                intermediates = intermediates[intermediates != col]
+
+                # Update cumulative distance
+                cumulative_distance = sum(
+                    distance_matrix.loc[row, intermediate] + distance_matrix.loc[intermediate, col]
+                    for intermediate in intermediates
+                )
+                distance_matrix.loc[row, col] = cumulative_distance
+
+    return distance_matrix
 
 def unroll_distance_matrix(df)->pd.DataFrame():
     """
@@ -26,9 +51,19 @@ def unroll_distance_matrix(df)->pd.DataFrame():
     Returns:
         pandas.DataFrame: Unrolled DataFrame containing columns 'id_start', 'id_end', and 'distance'.
     """
-    # Write your logic here
+    # Extract upper triangle of the distance matrix (excluding diagonal)
+    upper_triangle = distance_matrix.where(np.triu(np.ones(distance_matrix.shape), k=1).astype(bool))
 
-    return df
+    # Reset the index and stack the DataFrame to get a Series
+    stacked_series = upper_triangle.stack()
+
+    # Convert the Series to a DataFrame and reset the index
+    distance_df = stacked_series.reset_index()
+
+    # Rename the columns
+    distance_df.columns = ['id_start', 'id_end', 'distance']
+
+    return distance_df
 
 
 def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
@@ -43,10 +78,23 @@ def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
         pandas.DataFrame: DataFrame with IDs whose average distance is within the specified percentage threshold
                           of the reference ID's average distance.
     """
-    # Write your logic here
+    # Filter the DataFrame based on the reference_value
+    reference_df = df[df['id_start'] == reference_value]
 
-    return df
+    # Calculate the average distance for the reference_value
+    reference_avg_distance = reference_df['distance'].mean()
 
+    # Calculate the lower and upper thresholds within 10%
+    lower_threshold = reference_avg_distance - 0.1 * reference_avg_distance
+    upper_threshold = reference_avg_distance + 0.1 * reference_avg_distance
+
+    # Filter the DataFrame based on the thresholds
+    within_threshold_df = df[(df['distance'] >= lower_threshold) & (df['distance'] <= upper_threshold)]
+
+    # Get the unique values from the id_start column and sort them
+    result_ids = sorted(within_threshold_df['id_start'].unique())
+
+    return result_ids
 
 def calculate_toll_rate(df)->pd.DataFrame():
     """
@@ -58,7 +106,13 @@ def calculate_toll_rate(df)->pd.DataFrame():
     Returns:
         pandas.DataFrame
     """
-    # Wrie your logic here
+    # Define rate coefficients for each vehicle type
+    rate_coefficients = {'moto': 0.8, 'car': 1.2, 'rv': 1.5, 'bus': 2.2, 'truck': 3.6}
+
+    # Iterate over vehicle types and calculate toll rates
+    for vehicle_type, rate_coefficient in rate_coefficients.items():
+        # Calculate toll rates by multiplying distance with the rate coefficient
+        df[vehicle_type] = df['distance'] * rate_coefficient
 
     return df
 
@@ -73,6 +127,36 @@ def calculate_time_based_toll_rates(df)->pd.DataFrame():
     Returns:
         pandas.DataFrame
     """
-    # Write your logic here
+    # Define time ranges and discount factors
+    time_ranges = [
+        (time(0, 0, 0), time(10, 0, 0), 0.8),
+        (time(10, 0, 0), time(18, 0, 0), 1.2),
+        (time(18, 0, 0), time(23, 59, 59), 0.8)
+    ]
+    
+    weekend_discount_factor = 0.7
 
-    return df
+    # Create a new DataFrame to store the time-based toll rates
+    time_based_toll_rates_df = pd.DataFrame()
+
+    # Iterate over each time range
+    for start_time, end_time, discount_factor in time_ranges:
+        # Apply time range filters
+        mask = (df['start_time'] >= start_time) & (df['end_time'] <= end_time)
+        
+        # Apply discount factor to vehicle columns based on the time range
+        for vehicle_type in ['moto', 'car', 'rv', 'bus', 'truck']:
+            df.loc[mask, vehicle_type] *= discount_factor
+
+        # Append the result to the new DataFrame
+        time_based_toll_rates_df = time_based_toll_rates_df.append(df[mask])
+
+    # Apply constant discount factor for weekends
+    weekend_mask = (df['start_day'].isin(['Saturday', 'Sunday']))
+    for vehicle_type in ['moto', 'car', 'rv', 'bus', 'truck']:
+        df.loc[weekend_mask, vehicle_type] *= weekend_discount_factor
+
+    # Append the weekend results to the new DataFrame
+    time_based_toll_rates_df = time_based_toll_rates_df.append(df[weekend_mask])
+
+    return time_based_toll_rates_df
